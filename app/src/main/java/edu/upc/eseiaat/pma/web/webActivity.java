ca3056e404a357c8fab76ec6bf4bd0559d0b77cf
Jordi1985 <jordimarbar@gmail.com>
2017-10-09 13:31:37 +0200

    Initial  commit

--- app/src/main/java/edu/upc/eseiaat/pma/web/webActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/web/webActivity.java
@@ -2,6 +2,9 @@ package edu.upc.eseiaat.pma.web;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class webActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class webActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        editTextURL = (EditText) findViewById(R.id.editTextURL);
+        textViewesult = (TextView) findViewById(R.id.btn);
+
+    }
+
+
+    public void connect(View view) {
+        textViewResult.setText(WebReader.getUrl(editT))
+
     }
 }
